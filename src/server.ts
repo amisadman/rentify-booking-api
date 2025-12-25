1fed6460836ce0236cd222026f247ca37ec233dd
amisadman <inboxsadman@gmail.com>
2025-12-25 15:17:46 +0600

    chore: added dependencies

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+const app = express();
+app.use(express.json());
+app.use(morgan("combined"));
+
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "Hello from Rentify!!!",
+  });
+});
+app.listen(5000, () => {
+  console.log("Server is running on localhost 5000");
+});
