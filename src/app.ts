feb630f5b62f6a20f37b2f06910b7cf09627fdae
amisadman <inboxsadman@gmail.com>
2025-12-26 13:06:24 +0600

    feat: created routes

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,29 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
 import { initDB } from "./database/db";
+import { authRouter } from "./modules/auth/auth.routes";
+import { vehiclesRouter } from "./modules/vehicles/vehicles.routes";
+import { userRouter } from "./modules/user/user.routes";
+import { bookingRouter } from "./modules/booking/booking.routes";
+import { sendResponse } from "./utils/response";
 const app = express();
 app.use(express.json());
 app.use(morgan("combined"));
 initDB();
 
-
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/vehicles", vehiclesRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
 
 
 app.get("/", (req: Request, res: Response) => {
-  return res.status(200).json({
-    success: true,
-    message: "Hello from Rentify!!!",
-  });
+  return sendResponse(res,200,true,"Hello from Rentify!!!");
+});
+
+app.use((req: Request, res: Response) => {
+  return sendResponse(res, 404, false, "Route not found.");
 });
 
 export default app;
---
+++ src/utils/response.ts
@@ -0,0 +1,15 @@
+import { Response } from "express";
+
+export const sendResponse = (
+  res: Response,
+  status: number,
+  success: boolean,
+  message?: string,
+  data?: any
+) => {
+  return res.status(status).json({
+    success: success,
+    message: message,
+    data: data,
+  });
+};
